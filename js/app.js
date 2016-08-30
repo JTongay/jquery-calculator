533ce6fdea71a841b2196bd484cae3da362d2760
Joseph Tongay <dirk.tongay@gmail.com>
2016-08-29 20:54:27 -0500

    basic calculator works

--- js/app.js
+++ js/app.js
@@ -1,5 +1,41 @@
-$(function() {
+$(document).ready(function() {
+    //each number button is in a span tag with no id or class
+    //each operator is in a span tag with a class of operator
+    //all buttons are in a div container with a class of buttons
+    //the screen is a div with an id of screen
+    //the screen is wrapped in a div with an id of screen-container
+    //the entire body is wrapped in a container with an id of calculator
 
+    var $screen = $('#screen');
+    var $equals = $('#equals');
+    var $buttons = $('.buttons');
+
+    $(document).on('click', '#clear', function() {
+        $screen.text("")
+    })
+
+    $buttons.on('click', 'span', function() {
+        var $buttonPress = $(this).text();
+        if ($buttonPress === "÷") {
+            $screen.text($screen.text() + '/')
+        } else if ($buttonPress === "+") {
+            $screen.text($screen.text() + '+')
+        } else if ($buttonPress === "-") {
+            $screen.text($screen.text() + "-")
+        } else if ($buttonPress === "x") {
+            $screen.text($screen.text() + "*")
+        } else if ($buttonPress === "=") {
+            return;
+        } else {
+            $screen.text($screen.text() + $buttonPress)
+        }
+    })
+    $equals.on('click', function() {
+        var problem = $screen.text();
+        $screen.text(eval(problem))
+
+
+    })
 
 
 
